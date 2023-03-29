144a91c2bc5d21ef0ae59d86488aceed38f2edb4
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2023-03-29 16:43:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 level = false;
 objects = [];
+value = "";
+const synth = window.speechSynthesis;
 
 function setup() {
     canvas = createCanvas(480, 340);
@@ -16,6 +18,31 @@ function start() {
 
 function modelLoaded() {console.log('Model loaded'); level = true;}
 
+function gotResult() {
+    if(error) {console.error(error);} else {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function draw() {
     image(video, 0, 0, 480, 480);
+    if(level = true) {
+        for (let i = 0; i < array.length; i++) {
+            percent = Math.floor(objects[i].confidence) * 100;
+            text(objects[i].label + ": " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+
+
+            if(value == objects[i].label) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById('status').innerHTML = "Object found";
+                utterThis = new SpeechSynthesisUtterance('The object that was mentioned has been found.');
+                synth.speak(utterThis);
+            } else {
+                document.getElementById('status').innerHTML = "Object not found";
+                
+            }
+        }
+    }
 }
